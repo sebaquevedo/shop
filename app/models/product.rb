a76f0c65823d60d1af973f8682963941cd62eaf1
sebastian quevedo <sebaqd91@gmail.com>
2017-02-20 18:45:10 -0300

    changes on validations and callbacks before saving on Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+	belongs_to :category
+
+	validates :price, presence: true, numericality: { only_integer: true }
+	before_save :premium_default
+
+	def premium_default
+		self.premium ||= false
+=begin		unless self.premium.present? or self.premium == true
+			self.premium = false			
+		end
+=end	end
 end
