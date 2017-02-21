ec778cc0a039433e561c14a27bbac76cc808feba
sebastian quevedo <sebaqd91@gmail.com>
2017-02-21 15:19:14 -0300

    add scope on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
 	belongs_to :category
 
+	scope :premium_search, ->{ where(premium: true)}
+	scope :last_n, ->(cant) {order('id DESC').limit(cant)}
+
 	validates :price, presence: true, numericality: { only_integer: true }
 	before_save :premium_default
 
