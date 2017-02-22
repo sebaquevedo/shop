eabe48bdddce812f6b7b24eccaadbdabd917c764
sebastian quevedo <sebaqd91@gmail.com>
2017-02-22 02:23:29 -0300

    cambio category definicion valida porcentaje

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,8 +3,11 @@ class Category < ApplicationRecord
 	validate :discount_valid
 
 	def discount_valid
-		if discount<0 || discount>100
-			errors.add(:discount,"el descuento debe estar entre 0 y 100")
+		# if discount<0 || discount>100
+		# 	errors.add(:discount,"el descuento debe estar entre 0 y 100")
+		# end
+		if discount > 100 || discount < 0
+			errors.add(:discount, "El descuento debe ser un valor entre 0 y 100")
 		end
 	end
 
