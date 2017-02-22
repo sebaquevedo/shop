e5b5203d346c3c6f8ab87ce04507a67b5ef0f38d
sebastian quevedo <sebaqd91@gmail.com>
2017-02-22 02:13:38 -0300

    last modifications to category resolving heroku run rake db:seed

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
 	validate :discount_valid
 
 	def discount_valid
-		if discount < 0 || discount >100
+		if discount<0 || discount>100
 			errors.add(:discount,"el descuento debe estar entre 0 y 100")
 		end
 	end
